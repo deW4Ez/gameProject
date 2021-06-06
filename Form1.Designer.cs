6ba98b866cccdd91c0c1bf3944fcb59beb523df4
deW4Ez <ilkarik@gmail.com>
2021-06-06 11:18:23 +0500

    Initial commit 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace gameProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.labelScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelScore.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.Location = new System.Drawing.Point(895, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(171, 59);
+            this.labelScore.TabIndex = 0;
+            this.labelScore.Text = "Score: 0";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelScore.UseCompatibleTextRendering = true;
+            this.labelScore.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(300, 400);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(600, 200);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 3000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(292, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 236);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Конец игры";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1262, 977);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelScore);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 1024);
+            this.MinimumSize = new System.Drawing.Size(1280, 1024);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gameProject
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap PlayerShip = Resource.PlayerShip;
+        public List<Bullet> listBullets = new List<Bullet>();
+        private static Point PlayerLocation = new Point(400, 580);
+        private int _score = 0;
+
+        public List<Point> enemyPositions = new List<Point>();
+        public List<Enemy> enemies = new List<Enemy>();
+
+        public static int level = 1;
+        
+
+        //public Boss Boss = new Boss(new Point(350, 100));
+        public List<Bullet> BossBullet = new List<Bullet>();
+        public bool IsPlaying = false;
+        public bool IsPause = false;
+        public int lives = 5;
+        private int k = 1;
+
+        public List<Boss> listBoss = new List<Boss>();
+
+        public bool IsAlive = true;
+
+        public Form1()
+        {
+            InitializeComponent();            
+            CreateEnemies();            
+            this.KeyDown += new KeyEventHandler(input);
+            timer1.Start();
+        }
+
+        private void input(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Right:
+                    if (PlayerLocation.X < 880 && !IsPause)
+                        PlayerLocation.X+=20;
+                    break;
+                case Keys.Left:
+                    if (PlayerLocation.X-1 > 0 && !IsPause)
+                        PlayerLocation.X-=20;
+                    break;
+                case Keys.Space: 
+                    if(!IsPause)
+                        listBullets.Add(new Bullet(PlayerLocation,Resource.PixelBullet));
+                    break;
+                case Keys.Escape:
+                    if (IsPause) IsPause = false;
+                    else IsPause = true;
+                    break;
+            }
+        }
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(lives == 0)
+            {
+                IsPlaying = false;
+                label1.Visible = true;
+                label1.Text = "Конец игры. \n" + labelScore.Text;
+            }
+
+            if(!IsPause)
+            { 
+                foreach(var _bullet in listBullets)
+                {
+                    if (_bullet.IsAlive && _bullet.BulletLocation.Y > -100)
+                    {
+                        _bullet.BulletLocation.Y -= 20;
+                    }
+                }
+
+                foreach(var _boss in listBoss)
+                {
+                    if (_boss.IsAlive)
+                    {
+                        if (_boss.BossLocation.X < 100) k = 1;
+                        if (_boss.BossLocation.X > 800) k = -1;
+                        _boss.BossLocation.X += 10 * k;
+                    }
+                }
+
+                foreach(var _bulletEnemy in BossBullet)
+                {
+                    if(_bulletEnemy.IsAlive)
+                        _bulletEnemy.BulletLocation.Y += 20;
+                    if (_bulletEnemy.BulletLocation.Y > 900) _bulletEnemy.IsAlive = false;
+                }
+            }
+
+            
+
+            bool flag= false;
+            foreach (var _boss in listBoss)
+            {
+                if (_boss.IsAlive)
+                {
+                    foreach (var _bullet in BossBullet)
+                    {
+                        if (IsAlive && IsColide(PlayerLocation, _bullet.BulletLocation, new Size(110, 110)))
+                        {
+                            _bullet.IsAlive = false;
+                            BossBullet.Remove(_bullet);
+                            lives--;
+                            break;
+                        }
+                    }
+
+                    foreach (var _bullet in listBullets)
+                    {
+                        if (_boss.IsAlive && IsColide(_boss.BossLocation, _bullet.BulletLocation, _boss.Size))
+                        {
+                            _bullet.IsAlive = false;
+                            listBullets.Remove(_bullet);
+                            if (_boss.Lives == 0)
+                            {
+                                _boss.IsAlive = false;
+                                flag = true;
+                                AddScore(1000);
+                            }
+                            else
+                                _boss.Lives--;
+                            break;
+                        }
+                    }
+                }
+                if(flag) listBoss.Remove(_boss);
+                break;
+            }
+            
+
+            foreach (var enemy in enemies)
+            {
+                foreach (var _bullet in listBullets)
+                {
+                    if (enemy.IsAlive && IsColide(enemy.EnemyLocation, _bullet.BulletLocation, enemy.Size))
+                    {
+                        _bullet.IsAlive = false;
+                        listBullets.Remove(_bullet);
+                        enemy.IsAlive = false;
+                        AddScore(100);
+                        break;
+                    }                    
+                }
+                
+            }            
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int levelBots = level % 3;
+            if (IsPlaying)
+            {                
+                g.DrawImage(PlayerShip, new Rectangle(PlayerLocation.X, PlayerLocation.Y, 65, 95));
+
+                foreach (var _bullet in listBullets)
+                {
+                    if (_bullet.IsAlive && _bullet.BulletLocation.Y > -10)
+                        g.DrawImage(_bullet.Image, new Rectangle(_bullet.BulletLocation, new Size(20, 20)));
+                }
+                var deadCount = 0;
+
+                foreach (var enemy in enemies)
+                {
+                    if (enemy.IsAlive)
+                        g.DrawImage(enemy.Image, new Rectangle(enemy.EnemyLocation, new Size(70, 90)));
+                    else
+                        deadCount++;
+                }
+
+                for (int i = 1; i <= lives; i++)
+                    g.DrawImage(Resource.Heart, new Rectangle(new Point(50 * i, 50), new Size(50,50)));
+
+                foreach(var _boss in listBoss)
+                {
+                    _boss.IsAlive = true;
+                    g.DrawImage(_boss.Image, new Rectangle(_boss.BossLocation, new Size(100, 100)));
+                    foreach (var _bullet in BossBullet)
+                    {
+                        if (_bullet.IsAlive)
+                            g.DrawImage(_bullet.Image, new Rectangle(_bullet.BulletLocation, new Size(20, 20)));
+                    }
+                }
+
+                Random r = new Random();
+
+                if (levelBots == 0)
+                {
+                    listBoss.Add(new Boss(new Point(r.Next(200, 700), 100)));
+                    level++;
+                }
+
+                if (deadCount == 5)
+                {
+                    CreateEnemies();
+                    deadCount = 0;
+                    level++;
+                    timer2.Interval = (int)(3100 * (1 - 0.3 * (levelBots-1)));
+                }
+            }
+        }
+
+        public void CreateEnemies()
+        {
+            enemies.Clear();
+            enemyPositions.Clear();
+            Random r = new Random();
+            enemyPositions.Add(new Point(r.Next(0, 250), r.Next(0, 130)));
+            enemyPositions.Add(new Point(r.Next(260, 550), r.Next(0, 130)));
+            enemyPositions.Add(new Point(r.Next(550,850), r.Next(0, 130)));
+            enemyPositions.Add(new Point(r.Next(100, 400), r.Next(140, 270)));
+            enemyPositions.Add(new Point(r.Next(420, 700), r.Next(140, 270)));
+            foreach (var tempPoint in enemyPositions)
+                enemies.Add(new Enemy(tempPoint));
+            
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            labelScore.Text = "Score: " + _score.ToString();
+        }
+
+        private bool IsColide(Point enemyPoint, Point bulletPoint, Size SizeEnemy)
+        {
+            if (enemyPoint.X < bulletPoint.X && enemyPoint.X + SizeEnemy.Width > bulletPoint.X
+                && enemyPoint.Y-60  < bulletPoint.Y && enemyPoint.Y+SizeEnemy.Height > bulletPoint.Y)
+                return true;
+            return false;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            if (!IsPause)
+            {
+                foreach (var enemy in enemies)
+                {
+                    enemy.EnemyLocation.Y += (20);
+                    if (enemy.EnemyLocation.Y > 700 && enemy.IsAlive)
+                    {
+                        enemy.IsAlive = false;
+                        lives--;
+                    }
+                }               
+                
+            }
+
+            for (int i=0;i<listBullets.Count;i++)
+            {
+                if (listBullets[i].BulletLocation.Y == -10)
+                    listBullets.RemoveAt(i);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IsPlaying = true;            
+            this.Controls.Remove(sender as Button);
+            labelScore.Visible = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            foreach(var _boss in listBoss)
+                BossBullet.Add(new Bullet(new Point(_boss.BossLocation.X + 50, _boss.BossLocation.Y + 55), Resource.EnemyBullet));
+        }
+    }
+
+    public class Bullet
+    {
+        public Bitmap Image;
+        public Point BulletLocation = new Point();
+        public bool IsAlive = true;
+        public Bullet(Point PlayerLocation, Bitmap ImageBull)
+        {
+            BulletLocation = new Point(PlayerLocation.X+23,PlayerLocation.Y);
+            Image = ImageBull;
+        }
+    }
+    public class Enemy
+    {
+        public Bitmap Image = Resource.EnemyShip;
+        public Point EnemyLocation = new Point();
+        public bool IsAlive = true;
+        public Size Size = new Size(70, 90);
+        public Enemy(Point Location)
+        {
+            EnemyLocation = Location;            
+        }
+    }
+    public class Boss
+    {
+        public Bitmap Image = Resource.BossShip;
+        public Point BossLocation = new Point();
+        public bool IsAlive = true;
+        public int Lives = 10;
+        public Size Size = new Size(100, 110);
+        public Boss(Point Location)
+        {
+            BossLocation = Location;
+            IsAlive = true;
+        }
+    }
+}
